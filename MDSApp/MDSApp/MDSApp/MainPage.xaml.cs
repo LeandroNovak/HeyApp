7218fc11119030d48292aeefa4ab905240f95e68
Leandro Novak <leandro.novak@outlook.com>
2017-11-16 00:36:07 +0000

    create a simple layout

---
+++ MDSApp/MDSApp/MDSApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MDSApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post
+            {
+                Title = "Lorem Ipsum",
+                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam diam risus, malesuada non leo nec, volutpat tempus diam. Integer euismod pretium arcu sit amet malesuada. Sed id lacus massa. Curabitur aliquet justo in lectus hendrerit, sit amet rutrum diam sodales. Vivamus eget bibendum neque. Nulla facilisis, mi et pulvinar euismod, massa turpis congue neque, ut pretium est risus nec ipsum. In turpis risus, pretium ut est quis, fringilla dapibus enim. Nulla dapibus tristique ex nec placerat. Pellentesque eleifend magna quis tortor ultricies, eget mollis ligula pharetra. Aenean varius vehicula turpis sed porta. Sed sollicitudin tellus cursus vulputate cursus. ",
+                Picture = new Image() { Source = ImageSource.FromUri(new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hausziege_04.jpg/1200px-Hausziege_04.jpg")) },
+                Location = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+            };
+            Navigation.PushAsync(new DetailsPage(post));
+        }
+
+        private void OnClickNewPost(object sender, EventArgs e)
+        {
+
+            Navigation.PushAsync(new NewPost());
+        }
+    }
+}
---
+++ MDSApp/MDSApp/MDSApp/NewPost.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MDSApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewPost : ContentPage
+    {
+        private Post newPost;
+
+        public NewPost()
+        {
+            newPost = new Post();
+            InitializeComponent();
+        }
+
+        private void OnClickSendPost(object sender, EventArgs e)
+        {
+
+        }
+
+        private async Task OnClickAddPhotoAsync(object sender, EventArgs e)
+        {
+            var action = await DisplayActionSheet(null, "Cancel", null, "Select from Gallery", "Take a Photo");
+            Debug.WriteLine("Action: " + action);
+
+        }
+    }
+}
